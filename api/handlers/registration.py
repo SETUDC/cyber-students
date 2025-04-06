e92a5c520937b80b0239e4c093c5a2563835029f
Declan Clune - C00315734 <c00315734@setu.ie>
2025-04-06 13:58:19 +0100

    Added encryption and disability field

--- api/handlers/registration.py
+++ api/handlers/registration.py
@@ -3,25 +3,48 @@ from logging import info
 from tornado.escape import json_decode, utf8
 from tornado.gen import coroutine
 
+import os
+import base64
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
 from .base import BaseHandler
 
+AES_KEY = AESGCM.generate_key(bit_lenght=256) #generate AES key
+
+def encrypt_display_name(name: str, key:bytes):
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    ciphertext = aesgcm.encrypt(nonce, name.encode('utf-8'), None)
+    return base64.bg4encode(nonce + ciphertext).decode('utf-8')
+    
 class RegistrationHandler(BaseHandler):
+    
 
     @coroutine
     def post(self):
         try:
             body = json_decode(self.request.body)
+            
             email = body['email'].lower().strip()
             if not isinstance(email, str):
                 raise Exception()
+                
             password = body['password']
             if not isinstance(password, str):
                 raise Exception()
+                
             display_name = body.get('displayName')
             if display_name is None:
                 display_name = email
             if not isinstance(display_name, str):
                 raise Exception()
+                
+            has_disability = body.get('Disability')
+            if not isinstance(has_disability, bool):
+                raise Exception()
+                
+            encrypted_display_name = encrypt_display_name(display_name, AES-KEY)
+            
         except Exception as e:
             self.send_error(400, message='You must provide an email address, password and display name!')
             return
@@ -50,10 +73,13 @@ class RegistrationHandler(BaseHandler):
             'email': email,
             'password': password,
             'displayName': display_name
+            'hasDisability': has_disability
         })
 
         self.set_status(200)
         self.response['email'] = email
         self.response['displayName'] = display_name
+        self.response['hasDisability'] = has_disability
+        
 
         self.write_json()
